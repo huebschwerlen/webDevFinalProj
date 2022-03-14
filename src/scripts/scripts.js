81c32cbeac506bc4cd1e060ea6fa6f3c8f7acc39
Sam Huebschwerlen <huebschwerlen@gmail.com>
2022-03-14 13:02:32 -0700

    created function to dynamically change nav bar setting dependent on window size

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,5 @@
 // NAV BAR SECTION
+
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
 const menu = document.querySelector('nav .container ul');
@@ -17,6 +18,24 @@ closeBtn.addEventListener('click', () => {
   closeBtn.style.display = 'none';
 });
 
+const navItems = menu.querySelectorAll('li');
+
+//Nav items NON-active
+const changeActiveItems = () => {
+  navItems.forEach((item) => {
+    const link = item.querySelector('a');
+    link.classList.remove('active');
+  });
+};
+
+navItems.forEach((item) => {
+  const link = item.querySelector('a');
+  link.addEventListener('click', () => {
+    changeActiveItems();
+    link.classList.add('active');
+  });
+});
+
 // ABOUT ME SECTION
 // read more info btn in about section
 const readMoreBtn = document.querySelector('.read-more');
@@ -30,3 +49,17 @@ readMoreBtn.addEventListener('click', () => {
     readMoreBtn.textContent = 'Show more';
   }
 });
+
+//revert change nav depending on size on window
+const revertNav = () => {
+  if ($(window).width() > 1000) {
+    menu.style.display = 'flex';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'none';
+  } else {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+  }
+};
+window.onresize = revertNav;
