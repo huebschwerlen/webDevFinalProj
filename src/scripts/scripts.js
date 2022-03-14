a16987b63feaeeec307f690779e6e3f758761b49
Sam Huebschwerlen <huebschwerlen@gmail.com>
2022-03-14 12:07:24 -0700

    completed media query and added js for phone side nav bar

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,23 @@
+// NAV BAR SECTION
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+//Show nav menu
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'block';
+  menuBtn.style.display = 'none';
+  closeBtn.style.display = 'inline-block';
+});
+
+//Hide nav menu
+closeBtn.addEventListener('click', () => {
+  menu.style.display = 'none';
+  menuBtn.style.display = 'inline-block';
+  closeBtn.style.display = 'none';
+});
+
+// ABOUT ME SECTION
 // read more info btn in about section
 const readMoreBtn = document.querySelector('.read-more');
 const moreInfo = document.querySelector('.more-info');
